c5dd85d3f84c967e339d8e77f34d17c75d2aad67
yass-brenda <yass-brenda@users.noreply.github.com>
2022-01-03 21:22:30 +0000

    deploy: d9b89b989e7736fe02259209ea3193565d4e0631

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,13 +3,14 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
+const appNode = document.querySelector('#app')
 
 // web api
 // Conectarnos al servidor
 // Promise => async/await
 window
-    .fetch(url)
+    .fetch( `${baseUrl}/api/avo`)
     // Procesar la respuesta y  convertirla en JSON
     .then(res => res.json())
     // JSON -> Data -> Renderizar info en el browser
@@ -18,17 +19,21 @@ window
             responseJSON.data.forEach(item => {
                     // crear el titulo
                     const tittle = document.createElement('h2')
+                    tittle.textContent = item.name;
                     // crear la imagen
                     const image = document.createElement('img')
+                    image.src = `${baseUrl}${item.image}`;
                     // crear el parrafo
                     const price = document.createElement('div')
+                    price.textContent = item.price
+
                     // Contenedor para cada uno de los elementos
                     const container = document.createElement('div')
                     container.append(tittle,image,price)
 
                     todosItems.push(container)
             });
-            document.body.append(...todosItems)
+            appNode.append(...todosItems)
 
     });
 
